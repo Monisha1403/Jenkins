4bac4016ef056f8e25f4998c03f7cb8295c0899e
Monisha1403 <monishamurthy14@gmail.com>
2026-01-30 00:03:01 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 class SquareArea{
     public static void main(String[] args){
-        int side = 5;
+        int side = 7;
         int area = side * side;
         System.out.println("Area = " + area);
     }
